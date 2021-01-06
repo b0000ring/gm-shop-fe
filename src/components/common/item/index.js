b744c3c694fd04c8d7de0719aeae6eb6ae976c5d
booooring <booooring@yahoo.com>
2021-01-07 01:19:00 +0300

    main page done

--- src/components/common/item/index.js
+++ src/components/common/item/index.js
@@ -2,18 +2,31 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import styles from './index.module.css'
+import icon from './icon.svg'
 
-function Item({ id, image, name, price }) {
+function Item({ id, image, name, price, newPrice }) {
   return (
-    <Link href={`/item/${id}`} className={styles.item}>
-      <img src={image} className={styles.photo} />
-      <div className={styles.name}>
-        {name}
-      </div>
+    <div className={styles.item}>
+      <Link href={`/item/${id}`}>
+        <img src={image} className={styles.photo} />
+        <div className={styles.name}>
+          {name}
+        </div>
+      </Link>
       <div className={styles.price}>
-        {price}р
+        <div>
+          <span>
+            {newPrice || price} ₽
+          </span>
+          <span>
+            {newPrice ? `${price} ₽` : null}
+          </span>
+        </div>
+        <Link herf="#">
+          <img src={icon} />
+        </Link>
       </div>
-    </Link>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import clsx from 'clsx'
 
-import Layout from "src/components/layout"
+import Layout from 'src/components/layout'
 import SEO from "src/components/seo"
 import ItemsList from 'src/components/common/itemsList'
 import Banner from 'src/components/common/banner'
+
+import styles from './index.module.css'
 
 const IndexPage = ({ data }) => { 
   const collection = data.collection.edges[0].node.value
@@ -12,8 +15,50 @@ const IndexPage = ({ data }) => {
   return (
     <Layout> 
       <SEO title="Home" />
-      <Banner />
-      <ItemsList collection={collection} items={items} title="НОВЫЕ ПОСТУПЛЕНИЯ" />
+      <div className="content">
+        <ItemsList collection={collection} items={items} title="Новые поступления" />
+      </div>
+      <div className={styles.all}>
+        <Link href="/catalog">
+          ПОСМОТРЕТЬ ВСЕ ТОВАРЫ
+        </Link>
+      </div>
+      <div className="content">
+        <div className={styles.about}>
+          <div>
+            <h2>
+              Почему МИНИБИТ?
+            </h2>
+            <p>
+              Minibit - интернет-магазин портативной цифровой электроники и аксессуаров.
+              Мы работаем, чтобы сделать интересные и необычные устройства более доступными.
+              В магазине Minibit Вы можете приобрести редкие и интересующие Вас товары без длительного ожидания почтовой доставки из-за рубежа.<br /> 
+              Узнать подробности или задать вопросы Вы можете  по телефону или написав нам на почту. 
+            </p>
+            <Link href="/about">
+              Подробнее о нас
+            </Link>
+          </div>
+          <div>
+            <div className={styles.line}>
+              <div className={styles.section}>
+                <img src="/icons/location.svg" /> MINIBIT находится <br /> в Санкт-Петербурге
+              </div>
+              <div className={styles.section}>
+                <img src="/icons/percent.svg" /> Регулярные скидки <br /> и акции
+              </div>
+            </div>
+            <div className={styles.line}>
+              <div className={styles.section}>
+                <img src="/icons/truck.svg" /> Быстрая доставка <br /> по России
+              </div>
+              <div className={styles.section}>
+               <img src="/icons/check.svg" /> Чек и гарантия на все товары 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
